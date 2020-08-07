ce6c33b2d979c35746fed0f3eeaea0c8195acb10
Andrii Loburets <loburets.andrii@gmail.com>
2020-08-07 20:36:38 +0200

    Updates

---
+++ precache-manifest.c9fe100ed1d726704e099bdb484e0b80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5923a2a6ab6d4fc45454b963e6ff48ca",
+    "url": "/React-apps/index.html"
+  },
+  {
+    "revision": "48de6c2f495272a5d092",
+    "url": "/React-apps/static/css/main.e76c55fb.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/React-apps/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-apps/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48de6c2f495272a5d092",
+    "url": "/React-apps/static/js/main.216235d1.chunk.js"
+  },
+  {
+    "revision": "82c2d8404ddb49f1f227",
+    "url": "/React-apps/static/js/runtime-main.0e8074c4.js"
+  }
+]);
